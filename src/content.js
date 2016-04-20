2e7026f15380d5d44350711fe67359d2236ee214
Andrew Holloway <booc0mtaco@gmail.com>
2016-04-20 15:37:14 -0700

    clean up code in content/link_engine
    Fixes #12

--- src/content.js
+++ src/content.js
@@ -6,22 +6,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     // TODO: separate out the engine parts from the formatting parts
     if (request.action === 'parseImage') {
         // this is where we'll specifically handle getting the image URLs, etc.
+        // if the array contains rows, then send them back, or send back an empty header
         var content = performImage();
-        sendResponse(content);
+        sendResponse(content.length ? content.join('') : '<h1>No Images Present</h1>');
     } else if (request.action === 'sprintf') {
         // with this action, we only want to print things to console, or elsewhere
         console.log(request.message);
     }
 });
-
-var performImage = function() {
-    var imgs = document.getElementsByTagName('img'), t=[];
-    for (var i=0, n = imgs.length; i < n; i++) {
-        t.push('<a href="' + imgs[i].src + '"><img src="' + imgs[i].src + '" width="100%"></a>');
-    }
-    if (t.length) {
-        return t.join('');
-    } else {
-        return '<h2>No Images Present</h2>';
-    }
-};
--- src/link_engine.js
+++ src/link_engine.js
@@ -1,29 +1,19 @@
 var performImage = function() {
-    var t=[];
-    function prepareString(withUrl) {
-        return '<a href="' + withUrl + '"><img title="'+ withUrl +'" src="' + withUrl + '"></a>';
-    }
-
-    // Image tags are the easiest to do. Loop thru the set, and pull up the images out
-    Array.prototype.slice.call(document.getElementsByTagName('img')).forEach(function(imgTag) {
-        t.push(prepareString(imgTag.src));
-    });
-
-    // now inspect elements that are applied to background elements
-    var reImg = /^url\(\"/;
-    Array.prototype.slice.call(document.querySelectorAll('body *')).filter(function(element) {
+    // Try and find all image-looking resources on a page, and return a set
+    return Array.prototype.slice.call(document.querySelectorAll('body *')).filter(function(element) {
         // check for cases where no image is defined
         return window.getComputedStyle(element).backgroundImage !== 'none';
     }).filter(function(element) {
         // find elements that are inline images
-        return reImg.test(window.getComputedStyle(element).backgroundImage);
+        return /^url\(\"/.test(window.getComputedStyle(element).backgroundImage);
     }).map(function(element) {
         // trim CSS wrapping for URLs
         return window.getComputedStyle(element).backgroundImage.split('url("')[1].slice(0, -2);
-    }).forEach(function(url) {
-        // push to result array
-        t.push(prepareString(url));
+    }).concat(Array.prototype.slice.call(document.getElementsByTagName('img')).map(function(imgTag) {
+        // and merge in source values from raw image tags
+        return imgTag.src;
+    })).map(function(withUrl) {
+        // and format all the results into link/image sets
+        return '<a href="' + withUrl + '"><img title="'+ withUrl +'" src="' + withUrl + '"></a>';
     });
-
-    return t.length ? t.join('') : '<h1>No Images Present</h1>';
 };
