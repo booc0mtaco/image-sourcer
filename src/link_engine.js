b62497c84de86cca7af7ea25a544c4c0b3c67cc6
Andrew Holloway <booc0mtaco@gmail.com>
2016-04-20 12:52:18 -0700

    Update link engine to parse background image declarations
    Fixes #6

--- src/link_engine.js
+++ src/link_engine.js
@@ -1,11 +1,29 @@
 var performImage = function() {
-    var imgs = document.getElementsByTagName('img'), t=[];
-    for (var i=0, n = imgs.length; i < n; i++) {
-        t.push('<a href="' + imgs[i].src + '"><img title="'+ imgs[i].src +'" src="' + imgs[i].src + '" width="100%"></a>');
+    var t=[];
+    function prepareString(withUrl) {
+        return '<a href="' + withUrl + '"><img title="'+ withUrl +'" src="' + withUrl + '"></a>';
     }
-    if (t.length) {
-        return t.join('');
-    } else {
-        return '<h1>No IMG Tags Present</h1>';
-    }
+
+    // Image tags are the easiest to do. Loop thru the set, and pull up the images out
+    Array.prototype.slice.call(document.getElementsByTagName('img')).forEach(function(imgTag) {
+        t.push(prepareString(imgTag));
+    });
+
+    // now inspect elements that are applied to background elements
+    var reImg = /^url\(\"/;
+    Array.prototype.slice.call(document.querySelectorAll('body *')).filter(function(element) {
+        // check for cases where no image is defined
+        return window.getComputedStyle(element).backgroundImage !== 'none';
+    }).filter(function(element) {
+        // find elements that are inline images
+        return reImg.test(window.getComputedStyle(element).backgroundImage);
+    }).map(function(element) {
+        // trim CSS wrapping for URLs
+        return window.getComputedStyle(element).backgroundImage.split('url("')[1].slice(0, -2);
+    }).forEach(function(url) {
+        // push to result array
+        t.push(prepareString(url));
+    });
+
+    return t.length ? t.join('') : '<h1>No IMG Tags Present</h1>';
 };
