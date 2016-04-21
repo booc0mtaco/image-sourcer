84b45c16194743a9eaa00e866b039cc537dc51ec
Andrew Holloway <booc0mtaco@gmail.com>
2016-04-20 19:58:30 -0700

    removing superfluous comment

--- src/content.js
+++ src/content.js
@@ -3,7 +3,6 @@
 // once did.
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     // When getting the proper message, run the plugin code
-    // TODO: separate out the engine parts from the formatting parts
     if (request.action === 'parseImage') {
         // this is where we'll specifically handle getting the image URLs, etc.
         // if the array contains rows, then send them back, or send back an empty header
